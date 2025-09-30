95637f693aae4b39e3dd8fe5308d82a300aa74c2
DawnMagnet <axccjqh@qq.com>
2025-09-30 20:54:43 +0800

    fix bash scripts

--- PKGBUILD
+++ PKGBUILD
@@ -12,8 +12,10 @@ options=('!debug')
 _origin="https://cli.moonbitlang.com"
 source=("https://cli.moonbitlang.cn/binaries/latest/moonbit-linux-x86_64.tar.gz"
         "https://cli.moonbitlang.cn/cores/core-latest.tar.gz"
-        "moon.sh")
-sha256sums=($(./get_sha256sums.sh))
+        "moon.sh"
+        "get_version.sh"
+        "get_sha256sums.sh")
+sha256sums=($(./get_sha256sums.sh) 'SKIP' 'SKIP' 'SKIP')
 package() {
   install -Dm 755 "${srcdir}/bin/moon"     "${pkgdir}/usr/lib/moon"
   install -Dm 755 "${srcdir}/moon.sh"  "${pkgdir}/usr/bin/moon"
--- get_sha256sums.sh
+++ get_sha256sums.sh
@@ -9,13 +9,10 @@ curl -L https://cli.moonbitlang.cn/binaries/latest/moonbit-linux-x86_64.tar.gz -
 # Download the second source file
 curl -L https://cli.moonbitlang.cn/cores/core-latest.tar.gz -o "$tmpdir/core-latest.tar.gz"
 
-# Copy the local moon.sh file
-cp moon.sh "$tmpdir/moon.sh"
-
-# Calculate and output sha256sums in the order of the source array
+# Calculate and output sha256sums for the remote files only
 sha256sum "$tmpdir/moonbit-linux-x86_64.tar.gz" | cut -d' ' -f1
 sha256sum "$tmpdir/core-latest.tar.gz" | cut -d' ' -f1
-sha256sum "$tmpdir/moon.sh" | cut -d' ' -f1
 
 # Clean up the temporary directory
+rm -rf "$tmpdir"
 rm -rf "$tmpdir"
