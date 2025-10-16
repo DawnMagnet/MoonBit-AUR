7f14465cc0ed33936b42f055a0f445b21a5d2990
DawnMagnet <axccjqh@qq.com>
2025-10-16 21:24:11 +0800

    a

--- generate_PKGBUILD.sh
+++ generate_PKGBUILD.sh
@@ -13,13 +13,6 @@ tar -xzf "$tmpdir/moonbit-linux-x86_64.tar.gz" -C "$tmpdir"
 chmod +x "$tmpdir/bin/moon"
 version=$(cd "$tmpdir" && ./bin/moon version | grep 'moon ' | sed 's/.*moon \([0-9.]*\).*/\1/')
 
-# Download the core tar.gz file
-curl -L https://cli.moonbitlang.cn/cores/core-latest.tar.gz -o "$tmpdir/core-latest.tar.gz"
-
-# Calculate sha256sums
-sha1=$(sha256sum "$tmpdir/moonbit-linux-x86_64.tar.gz" | cut -d' ' -f1)
-sha2=$(sha256sum "$tmpdir/core-latest.tar.gz" | cut -d' ' -f1)
-
 # Generate PKGBUILD from template using safe delimiter |
 if [ ! -f PKGBUILD.template ]; then
 	echo "PKGBUILD.template not found in repository. Aborting."
@@ -27,12 +20,12 @@ if [ ! -f PKGBUILD.template ]; then
 	exit 1
 fi
 
-sed "s|__VERSION__|$version|g; s|__SHA1__|$sha1|g; s|__SHA2__|$sha2|g" PKGBUILD.template > PKGBUILD
+# Generate PKGBUILD from template using safe delimiter |
+# Only substitute the version placeholder; SHA placeholders were removed per request
+sed "s|__VERSION__|$version|g" PKGBUILD.template > PKGBUILD
 
 
 echo "Generated PKGBUILD with version: $version revision: 1"
-echo "sha256 of moonbit-linux-x86_64.tar.gz: $sha1"
-echo "sha256 of core-latest.tar.gz: $sha2"
 
 # Clean up
 rm -rf "$tmpdir"
